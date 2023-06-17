55a5a3d77084d12fad42480ef4ab1d314c0b3ac7
bobevski96 <106536988+bobevski96@users.noreply.github.com>
2023-06-17 11:00:57 +0200

    Add files via upload

---
+++ HomeWork03/homework03/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CharacterPage from "./Components/CharactersPage/CharactersPage.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <CharacterPage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ HomeWork03/homework03/src/Components/CharactersPage/CharactersPage.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import Character from "../Character/Character.jsx"
+export default class CharacterPage extends Component {
+    URL="https://swapi.dev/api/people";
+  constructor() {
+      super();
+    this.state = {
+      characters: [],
+    };
+  }
+
+  getData=async()=>{
+    const response = await fetch(this.URL)
+    const data = await response.json()
+    this.setState({
+      characters:data.results
+    })
+    console.log(this.state.characters)
+  };
+   
+    
+ componentDidMount() {
+   this.getData()
+    console.log("COMPONENT DID MOUNT");
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Home Work 03</h3>
+        {this.state.characters.map((character)=>{
+          return<Character key={character.id} data={character}/>
+        })}
+      </div>
+    );
+  }
+}
